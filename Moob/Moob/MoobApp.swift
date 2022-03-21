364a345d7fccd481e9091f45e7da3c46d663d5dc
노연주 <yjnoh1016@naver.com>
2022-03-21 20:53:38 +0900

    [CREATE] 프로젝트 재생성

---
+++ Moob/Moob/MoobApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoobApp.swift
+//  Moob
+//
+//  Created by 노연주 on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MoobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
