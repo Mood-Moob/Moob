98b5a6b4d0468576f6b478a9a1aad1745bb2716f
노연주 <yjnoh1016@naver.com>
2022-04-25 14:17:52 +0900

    [CREATE] TopView Created

---
+++ Moob/Moob/TopView.swift
@@ -0,0 +1,37 @@
+//
+//  TopView.swift
+//  Moob
+//
+//  Created by 노연주 on 2022/04/25.
+//
+
+import SwiftUI
+
+struct TopView: View {
+    var body: some View {
+        ZStack{
+            VStack(alignment: .leading){
+                HStack{
+                    Button(action: {print("Prev")}){
+                        Image("Moob_Prev").padding().hidden()
+                    }
+                    Spacer()
+                    Button(action: {print("Home")}){
+                        Image("Moob_Home").padding().hidden()
+                    }
+                }
+                VStack(alignment: .leading){
+                    Image("Moob_Logo")
+                    Text("Choose one and try it.").font(.system(size: 20, weight: .thin, design: .default)).foregroundColor(Color(red: 125/255, green: 125/255, blue: 125/255))
+                }.padding().offset(x: 20, y: -20)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct TopView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopView()
+    }
+}
