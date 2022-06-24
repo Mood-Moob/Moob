39aac20a4ac02aa22c3ef0b05746996d3b7a7e34
노연주 <yjnoh1016@naver.com>
2022-06-24 22:04:01 +0900

    [CREATE] MainView

---
+++ Moob/Moob/MainView.swift
@@ -0,0 +1,37 @@
+//
+//  MainView.swift
+//  Moob
+//
+//  Created by 노연주 on 2022/04/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                TopView()
+                VStack{
+                    NavigationLink(destination: ChooseMbtiView().navigationBarHidden(true)){
+                        MenuView(descriptionText: "I know my MBTI", image: "Moob_KnowMyMBTI")
+                            .cornerRadius(15)
+                            .shadow(color: Color.black.opacity(0.25), radius: 4, x: 2, y: 2).padding(25)
+                    }
+                    
+                    NavigationLink(destination: WebView(urlToLoad: "https://www.16personalities.com/ko")){
+                                                MenuView(descriptionText: "Test my MBTI", image: "Moob_TestMBTI")
+                                                    .cornerRadius(15)
+                                                    .shadow(color: Color.black.opacity(0.25), radius: 4, x: 2, y: 2).padding(25)
+                                        }
+                }.offset(y: -150)
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
