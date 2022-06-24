d92d8e2a3ad528c9b592622e2f0fd25e1946ca3a
노연주 <yjnoh1016@naver.com>
2022-06-24 22:04:16 +0900

    [CREATE] ChooseMbtiView

---
+++ Moob/Moob/ChooseMbtiView.swift
@@ -0,0 +1,20 @@
+//
+//  ChooseMbtiView.swift
+//  Moob
+//
+//  Created by 노연주 on 2022/04/25.
+//
+
+import SwiftUI
+
+struct ChooseMbtiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChooseMbtiView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChooseMbtiView()
+    }
+}
