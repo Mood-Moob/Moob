dec82c1b791dafde882c1ea8e3f00724c6e81cb3
노연주 <yjnoh1016@naver.com>
2022-06-24 22:04:35 +0900

    [UPDATE] ContentView MainView

--- Moob/Moob/ContentView.swift
+++ Moob/Moob/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding().font(.custom("SigmarOne-Regular", size: 15))
+        MainView()
     }
 }
 
